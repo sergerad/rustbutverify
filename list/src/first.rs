99447f59c70612d0d858f996d3828657fd98d553
sergerad <serge.radinovich@immutable.com>
2024-06-11 19:47:04 +1200

    generic list and peek

--- list/src/first.rs
+++ list/src/first.rs
@@ -54,3 +54,37 @@ impl Drop for List {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::default();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
--- list/src/second.rs
+++ list/src/second.rs
@@ -1,22 +1,17 @@
-pub struct List {
-    pub head: Link,
+#[derive(Default)]
+pub struct List<T> {
+    pub head: Link<T>,
 }
 
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-pub struct Node {
-    pub elem: i32,
-    pub next: Link,
+pub struct Node<T> {
+    pub elem: T,
+    pub next: Link<T>,
 }
 
-impl std::default::Default for List {
-    fn default() -> Self {
-        List { head: None }
-    }
-}
-
-impl List {
-    pub fn push(&mut self, elem: i32) {
+impl<T> List<T> {
+    pub fn push(&mut self, elem: T) {
         // Create a new head which points to the old head
         let new_head = Box::new(Node {
             elem,
@@ -27,16 +22,24 @@ impl List {
         self.head = Some(new_head);
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             // Set current head to old's next and return value
             self.head = node.next;
             node.elem
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
 }
 
-impl Drop for List {
+impl<T> Drop for List<T> {
     fn drop(&mut self) {
         // Take head
         let mut link = self.head.take();
@@ -45,5 +48,56 @@ impl Drop for List {
         while let Some(mut node) = link {
             link = node.next.take();
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::default();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::default();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        list.peek_mut().map(|value| *value = 42);
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
     }
 }
