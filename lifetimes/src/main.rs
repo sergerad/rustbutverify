217ffddbe77ad858e65eee4eef69b31b292062b7
sergerad <serge.radinovich@immutable.com>
2025-01-08 15:05:44 +1300

    Update lifetimes

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,23 +1,50 @@
-// From https://doc.rust-lang.org/book/ch10-03-lifetime-syntax.html
+#[derive(Debug)]
+struct Data {
+    a: String,
+}
 
-// Specifies that the returned reference will have the same lifetime
-// as the shorter of the two input lifetimes.
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
+impl Data {
+    fn new(a: String) -> Data {
+        Data { a }
+    }
+}
+
+#[derive(Debug)]
+struct Datastore {
+    data: Vec<Data>,
+}
+
+impl Datastore {
+    fn new() -> Datastore {
+        Datastore { data: Vec::new() }
+    }
+
+    fn add(&mut self, data: Data) {
+        self.data.push(data);
+    }
+
+    fn get(&self, index: usize) -> &Data {
+        &self.data[index]
     }
 }
 
 fn main() {
-    let string1 = String::from("long string is long");
-    // Is set to string2 subsequently.
-    let result;
-    {
-        let string2 = String::from("xyz");
-        // Does not compile because lifetime of string2 is too short.
-        result = longest(string1.as_str(), string2.as_str());
-    }
-    println!("The longest string is {result}");
+    // Get a mutable reference to the Datastore
+    let mut ds = Datastore::new();
+    let d = Data::new("hello".to_string());
+    ds.add(d);
+
+    // Derive some data from the Datastore. The lifetime of which is
+    // implicitly tied to the lifetime of the Datastore
+    let dd_a = {
+        // We need this shared reference to be out of scope before
+        // we use the mutable reference of Datastore again
+        let dd = ds.get(0);
+        dd.a.clone()
+    };
+    println!("{:?}", dd_a);
+
+    // We can do this because the shared reference is out of scope
+    let ddd = Data::new("world".to_string());
+    ds.add(ddd);
 }
